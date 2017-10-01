86ff9f0db4e3167fbad435940d877551d1dcde6c
Jordi Martínez Frias <jordimart83@gmail.com>
2017-10-01 22:47:10 +0200

    [ADD] Configuraciones de Auth service.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,19 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor( private auth:AuthService ) { }
+  constructor(private auth: AuthService) {
+    auth.handleAuthentication();
+  }
 
   ngOnInit() {
   }
 
-  login(){
+  login() {
     this.auth.login();
+  }
+
+  salir() {
+    this.auth.logout();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,8 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'https://jordimart83.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid',
+    language: 'es'
   });
 
   constructor(public router: Router) {}
@@ -20,5 +21,43 @@ export class AuthService {
   public login(): void {
     this.auth0.authorize();
   }
+
+  public handleAuthentication(): void {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        this.setSession(authResult);
+        this.router.navigate(['/home']);
+      } else if (err) {
+        this.router.navigate(['/home']);
+        console.log(err);
+      }
+    });
+  }
+
+  private setSession(authResult): void {
+    // Set the time that the access token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  public logout(): void {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // Go back to the home route
+    this.router.navigate(['/']);
+  }
+
+  public isAuthenticated(): boolean {
+    // Check whether the current time is past the
+    // access token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
 
 }
